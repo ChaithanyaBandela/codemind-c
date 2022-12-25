5987b7edf42a9a6735c8df0252ac30734ea487d1
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2022-12-25 21:32:08 +0530

    #include<stdio.h> int main() {     int a,b,i,sum=0,d,e,j,f,g=0;     scanf("%d",&a);     b=a*a;     for(i=a;i>0;)     {         d=i%10;         sum=(sum*10)+d;         i=i/10;     }     e=sum*sum;     for(j=e;j>0;)     {         f=j%10;         g=(g*10)+f;         j=j/10;     }     if(g==b)     printf("True");     else     printf("False"); }

--- Adam_number.c
+++ Adam_number.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,i,c=0,d,e,j,g=0,h;
+    int a,b,i,sum=0,d,e,j,f,g=0;
     scanf("%d",&a);
     b=a*a;
     for(i=a;i>0;)
     {
         d=i%10;
-        c=(c*10)+d;
+        sum=(sum*10)+d;
         i=i/10;
     }
-    e=c*c;
+    e=sum*sum;
     for(j=e;j>0;)
     {
-        h=j%10;
-        g=(g*10)+h;
+        f=j%10;
+        g=(g*10)+f;
         j=j/10;
     }
     if(g==b)
