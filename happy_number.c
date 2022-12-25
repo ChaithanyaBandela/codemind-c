e1a37194a6feef136e404ea50135ac4f6eb92d07
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2022-12-25 12:06:58 +0530

    #include<stdio.h> #include<math.h> int main() {     int i,j,temp,sum=0,num;     scanf("%d",&num);     while(sum!=1&&sum!=4)     {         sum=0;         while(num>0)         {             j=num%10;             sum=sum+(j*j);             num=num/10;         }         num=sum;     }     if(sum==1)     printf("True");     else     printf("False"); }

---
+++ happy_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i,j,temp,sum=0,num;
+    scanf("%d",&num);
+    while(sum!=1&&sum!=4)
+    {
+        sum=0;
+        while(num>0)
+        {
+            j=num%10;
+            sum=sum+(j*j);
+            num=num/10;
+        }
+        num=sum;
+    }
+    if(sum==1)
+    printf("True");
+    else
+    printf("False");
+}
