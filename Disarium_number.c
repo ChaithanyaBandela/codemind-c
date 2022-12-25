86b567be9945f7185de3a002e41d4d62e9a404ee
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2022-12-25 20:51:23 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,i,sum=0,rem=0;     scanf("%d",&a);     i=a;     while(i>0)     {         sum=sum+1;         i=i/10;     }     i=a;     while(i>0)     {         rem=rem+pow(i%10,sum);         i=i/10;         sum--;     }     if(rem==a)     printf("True");     else     printf("False"); }

--- Disarium_number.c
+++ Disarium_number.c
@@ -1,35 +1,24 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int n,r,s=0,t,c=1,i,p=1;
-    scanf("%d",&n);
-    t=n;
-    while(t!=0)
+    int a,i,sum=0,rem=0;
+    scanf("%d",&a);
+    i=a;
+    while(i>0)
     {
-        r=t%10;
-        s=s*10+r;
-        t=t/10;
+        sum=sum+1;
+        i=i/10;
     }
-    t=s;
-    s=0;
-    while(t!=0)
+    i=a;
+    while(i>0)
     {
-        r=t%10;
-        for(i=1;i<=c;i++)
-        {
-            p=p*r;
-        }
-        s=s+p;
-        t=t/10;
-        c++;
-        p=1;
+        rem=rem+pow(i%10,sum);
+        i=i/10;
+        sum--;
     }
-    if(s==n)
-    {
-        printf("True");
-    }
+    if(rem==a)
+    printf("True");
     else
-    {
-        printf("False");
-    }
+    printf("False");
 }
