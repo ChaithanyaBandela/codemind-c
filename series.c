fa049cfa9376576a8cc826f02bffb33a0b837ec7
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2022-12-29 22:22:17 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,x=1,y=1,i,a,b;     scanf("%d",&n);     for(i=1;i<=n+1;i++)     {         if(i%2!=0)         {           a=(pow(2,x))-1;           printf("%d ",a);           x++;         }         else         {             b=(pow(3,y))-1;             printf("%d ",b);             y++;         }     } }

--- series.c
+++ series.c
@@ -2,20 +2,19 @@
 #include<math.h>
 int main()
 {
-    int n,x=0,y=0,i,a,b;
-    printf("0 ");
+    int n,x=1,y=1,i,a,b;
     scanf("%d",&n);
-    for(i=1;i<n;i++)
+    for(i=1;i<=n+1;i++)
     {
         if(i%2!=0)
         {
-          a=pow(3,x);
+          a=(pow(2,x))-1;
           printf("%d ",a);
           x++;
         }
         else
         {
-            b=pow(2,y);
+            b=(pow(3,y))-1;
             printf("%d ",b);
             y++;
         }
